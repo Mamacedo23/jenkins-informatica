e7b719fd5b5be68d692fc715c8240d86036871a6
Kevin <kevin.gonzales.m@uni.pe>
2026-07-10 10:19:59 -0500

    Modified saludo.sh 2

--- saludo.sh
+++ saludo.sh
@@ -1,2 +1,3 @@
 echo "Hola Misaka Mikoto, ¿Te casarías conmigo?"
 echo "Sería el sueño de todo hombre"
+echo "Si me dices que sí, te prometo que te haré feliz todos los días de nuestra vida juntos."
